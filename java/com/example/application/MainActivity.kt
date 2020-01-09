b255c3cef8c22d0aaaad899141856b6e7e4f8f65
parkjungkwan <pakjkwan@gmail.com>
2020-01-09 11:04:37 +0900

    11:04

--- java/com/example/application/MainActivity.kt
+++ java/com/example/application/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.application
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn.setOnClickListener{
+            //Toast.makeText(this,"click",Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
     }
 }
